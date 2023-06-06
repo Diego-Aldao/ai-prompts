dddb84433efb3856da219fda6ce51b7c90d9f3d7
Diego Aldao <diego_ind@live.com.ar>
2023-06-06 18:28:56 -0300

    añadido el contenido y el componente feed

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,23 @@
+import Feed from "@components/Feed";
 import React from "react";
 
 const Inicio = () => {
-  return <div>Inicio</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center capitalize">
+        descrubre y comparte
+        <span className="orange_gradient text-center block">
+          prompts impulsados por IA
+        </span>
+      </h1>
+      <p className="desc text-center">
+        PromptVerse es una herramienta de prompts de inteligencia artificial de
+        código abierto para que el mundo moderno descubra, cree y comparta
+        prompts creativos.
+      </p>
+      <Feed />
+    </section>
+  );
 };
 
 export default Inicio;
